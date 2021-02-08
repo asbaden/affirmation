de2cdbbca5e3379f326a06cc9db803fe05d6169f
asbaden <gusbaden96@gmail.com>
2021-02-07 22:45:42 -0800

    continuing with tutorial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
+import "./index.css"
 
-function Greeting (){
-  return <h4>This is Gus and this is not my first component</h4>;
+
+
+
+
+
+// stateless funcitonal component 
+// always return JSX
+
+
+//JSX RULES
+// return single element 
+// div / section / article or Fragment
+// use camelCase for html attribute 
+// className instead of classclose every element 
+// formatting
+
+
+function BookList() {
+  return (
+  <section className="bookList">
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+  </section>
+  )
+}
+const Book = () => {
+  return (
+  <article className="book">
+    <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt=""/>
+    
+    <h1>I love you to the moon and back</h1>
+    <h4>Amelia Hepworth</h4>
+  </article>
+  )
 }
 
 
-ReactDOM.render(<Greeting />,document.getElementById('root'))
+ReactDOM.render(<BookList />,document.getElementById('root'));
