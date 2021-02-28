4779607423eadf902f4b89e4b411ab627289331e
asbaden <gusbaden96@gmail.com>
2021-02-27 21:56:49 -0800

    pushing through

--- src/index.js
+++ src/index.js
@@ -18,22 +18,47 @@ import "./index.css"
 // use camelCase for html attribute 
 // className instead of classclose every element 
 // formatting
-
-const author = "Amelia Hepworth";
-const title = "I Love You to the Moon and Back";
-const img = "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+const books = [
+  {
+    id:1,
+    img:"https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title:"I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id:2,
+    img:"https://images-na.ssl-images-amazon.com/images/I/71eJk7GtUOL._AC_UL200_SR200,200_.jpg",
+    title:"The Boy, the Mole, the Fox and the Horse",
+    author: "Charlie Mackesy",
+  },
+  {
+    id:3,
+    img:"https://m.media-amazon.com/images/I/51k4fKV2RxL.jpg",
+    title:"Harry Potter et la Chambre des Secrets ",
+    author:"J.K. Rowling",
+  }
+]
 
 function BookList() {
+  
   return (
   <section className="bookList">
-    <Book job="developer" title="rnadom title" number={22}/>
-    
+   {books.map((book)=>{
+     return (
+      <Book key={book.id} {...book}></Book>
+     )
+   })}
   </section>
   )
 }
 
 
+
 const Book = (props) => {
+  console.log(props);
+  
+  const {img, title, author} = props
+  
   
   return (
   <article className="book">
